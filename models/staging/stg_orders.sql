d8bc60cfb56564f3f5f4ebb0a68f37a4a111d935
Sandhya Chippagiri <sandhya.chippagiri@infometry.net>
2026-01-19 08:30:05 +0000

    commiting in front of Balaji sir.

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,12 +1,10 @@
-
 {{ config(materialized='view') }}
-
 
 select
     order_id,
     customer_id,
-    {{ clean_string('order_date', 'date', default_order_date) }} as order_date,
-    {{ clean_string('status', 'string', default_status) }} as status,
+    {{ clean_string('order_date', 'date', var('default_order_date')) }} as order_date,
+    {{ clean_string('status', 'string', var('default_status')) }} as status,
     {{ clean_string('total_amount', 'number') }} as total_amount,
     {{ clean_string('updated_at', 'timestamp') }} as updated_at
 from {{ source('sf', 'orders') }}
